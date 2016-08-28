afa25b5346d15ad131c992e0e15fcd236e284871
anzurio <anzurio@gmail.com>
2016-08-28 15:36:59 -0500

    A new customer walks in
    A customer really liked the DataDisplayer and wants to use it, however, their data store is different and such, we need to use a different third party driver to access it. Furthermore, the new driver's API is vastly different.

---
+++ InversionOfControlTutorial/ControladorDeDatos/Conexion.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// A third party library to access the Customer 2's Data Store
+/// </summary>
+namespace ControladorDeDatos
+{
+    /// <summary>
+    /// The main class uses to access the data.
+    /// </summary>
+    public class Conexion
+    {
+        string _protocol;
+        string _address;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="protocol">The protocol of the data store (e.g., mongodb).</param>
+        /// <param name="address">The address of the data store.</param>
+        public Conexion(string protocol, string address)
+        {
+            _protocol = protocol;
+            _address = address;
+        }
+
+        /// <summary>
+        /// Method used to retrieve data from the data store.
+        /// </summary>
+        /// <param name="data">The data retrieved.</param>
+        public void ObtenerDatos(out string data)
+        {
+            data = _protocol + _address + ",uno,dos,tres";
+        }
+    }
+}
